209df75d0a74ec4d7a95978ee4d1de25001d9c36
Amarnath S S <amarnathss306@gmail.com>
2025-03-16 14:59:33 +0530

    initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="">
+      App
+      <h1 class="text-3xl font-bold underline bg-red-300">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
