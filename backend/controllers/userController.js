a0795d1351045e7114889fce75b12e9c8be9bbd2
Amarnath S S <amarnathss306@gmail.com>
2025-03-17 22:44:00 +0530

    3

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -41,7 +41,83 @@ const createUser = asyncHandler(async (req,res)=>{
 const loginUser = asyncHandler(async (req,res)=>{
     const {email,password} = req.body;
     const existingUser = await User.findOne({email})
-    console.log(existingUser)
+    if(existingUser){
+        const isPasswordValid = await bcrypt.compare(password,existingUser.password)
+        if(isPasswordValid){
+            createToken(res,existingUser._id);
+            res.status(201).json({
+                _id:existingUser._id,
+                username:existingUser.username,
+                email:existingUser.email,
+                isAdmin:existingUser.isAdmin
+            });
+        }
+        else{
+            res.status(401).json({message:"invalid password"});
+        }
+    }
+    else{
+        res.status(401).json({message:"user not found"});
+    }
+    
 
 })
-export {createUser,loginUser};
+
+const logoutCurrentUser = asyncHandler(async (req,res) =>{
+    res.cookie('jwt','',{
+        httpOnly : true,
+        expires:new Date(0)
+    })
+
+    res.status(200).json({message:"loged out successfully"})
+})
+
+const getAllUsers = asyncHandler(async (req,res)=>{
+    const users = await User.find({})
+    res.json(users)
+})
+
+const getCurrentUserProfile = asyncHandler(async (req,res)=>{
+    const user = await User.findById(req.user._id)
+    if(user){
+        res.json({
+            _id : user._id,
+            username:user.username,
+            email:user.email
+        })
+    }
+    else{
+        res.status(404)
+        throw new Error("user not found.");
+    }
+
+} )
+
+const updateCurrentUserProfile = asyncHandler(async (req,res)=>{
+    const user = await User.findById(req.user._id)
+
+    if(user){
+        user.username = req.body.username || user.username;
+        user.email  = req.body.email || user.email;
+
+        if(req.body.password){
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(req.body.password,salt)
+            user.password = hashedPassword;
+        }
+        const updatedUser = await user.save()
+        res.json({
+            _id:updatedUser._id,
+            username:updatedUser.username,
+            email:updatedUser.email,
+            isAdmin:updatedUser.isAdmin
+        })
+    }
+    else{
+        res.status(404)
+        throw new Error("user not found");
+    }
+
+})
+
+export {createUser,loginUser ,logoutCurrentUser,getAllUsers , getCurrentUserProfile ,updateCurrentUserProfile};
