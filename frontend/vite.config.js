2a969421553bd6765af8c8b2bd0af49bb5f756be
Amarnath S S <amarnathss306@gmail.com>
2025-05-02 21:57:02 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,62 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-  server:{
-    proxy: {
-      '/api/': {
-        target:  import.meta.env.VITE_API_URL || 'http://localhost:3000', // Your backend
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load .env.[mode] file
+  const env = loadEnv(mode, process.cwd())
+
+  // Always access like this:
+  const apiUrl = env.VITE_API_URL || 'http://localhost:3000';
+
+  return {
+    plugins: [
+      tailwindcss(),
+      react()
+    ],
+    server: {
+      proxy: {
+        '/api/': {
+          target: apiUrl,
+          changeOrigin: true,
+          secure: false,
+        },
+        '/uploads/': {
+          target: apiUrl,
+          changeOrigin: true,
+          secure: false,
+        },
       },
-      '/uploads/': {
-        target: import.meta.env.VITE_API_URL || 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    }
   }
 })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//     react()
+//   ],
+//   server:{
+//     proxy: {
+//       '/api/': {
+//         target:  import.meta.env.VITE_API_URL || 'http://localhost:3000', // Your backend
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//       '/uploads/': {
+//         target: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   }
+// })
+
+
