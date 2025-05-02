1e887b6d7bed66370cdaaca3e52aa63bc855ced3
Amarnath S S <amarnathss306@gmail.com>
2025-05-02 21:52:08 +0530

    update for deployment

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,18 @@ connectDB();
 
 const app = express()
 
+
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials : true,
-}))
+  origin: [
+    'http://localhost:5173', 
+    'https://your-frontend-domain.vercel.app'
+  ],
+  credentials: true,
+}));
 
 const PORT = process.env.PORT||3000;
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,12 +11,12 @@ export default defineConfig({
   server:{
     proxy: {
       '/api/': {
-        target: 'http://localhost:3000', // Your backend
+        target:  import.meta.env.VITE_API_URL || 'http://localhost:3000', // Your backend
         changeOrigin: true,
         secure: false,
       },
       '/uploads/': {
-        target: 'http://localhost:3000',
+        target: import.meta.env.VITE_API_URL || 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       },
